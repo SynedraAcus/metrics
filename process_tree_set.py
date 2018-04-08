4c946dc06213d7785667b99b790bafa766f849f1
SynedraAcus <alexeymorozov1991@gmail.com>
2018-04-08 12:51:13 +0800

    Removed flu file & minor fixes

--- process_tree_set.py
+++ process_tree_set.py
@@ -2,8 +2,7 @@
 
 from argparse import ArgumentParser
 from dendropy import TreeList
-from metrics import get_rooted_vector, get_unrooted_vector, \
-        annotate_rooted_tree, annotate_unrooted_tree
+from metrics import annotate_rooted_tree, annotate_unrooted_tree
 from multiprocessing import Pool
 from os import getpid, cpu_count
 from sys import stderr
@@ -13,8 +12,6 @@ from time import time
 def write_tree(tree, func, filename):
     """
     Get a vector for a given tree and write it into a file.
-    
-    A separate function for multiprocessing later.
     :param tree:
     :return:
     """
@@ -40,7 +37,7 @@ def write_tree(tree, func, filename):
             
 parser = ArgumentParser('Return CP- or CPM-vectors for a set of trees\n'+
                         'The vectors are written to a separate file each,\n'+
-                        'named {tree_file}.tree{tree_number}.vector')
+                        'named {tree_file}.tree_{tree_number}.vector')
 parser.add_argument('-t', type=str, help='Tree file in Newick format')
 parser.add_argument('-u', action='store_true',
                     help='Produce unrooted (CPM) labelling')

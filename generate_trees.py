a2317a6e166554ae05a329094fc872a4108c874b
SynedraAcus <alexeymorozov1991@gmail.com>
2018-06-12 18:58:25 +0800

    A tree generator

---
+++ generate_trees.py
@@ -0,0 +1,28 @@
+#! /usr/bin/python3
+
+from argparse import ArgumentParser
+from dendropy import TreeList, TaxonNamespace
+from dendropy.simulate import treesim
+import os
+
+parser = ArgumentParser('Generate trees of a given size with different algos')
+parser.add_argument('-n', type=int, help='Tree size', default=100)
+parser.add_argument('-d', type=str, help='Output directory')
+args = parser.parse_args()
+
+if not os.path.isdir(args.d):
+    os.mkdir(args.d)
+os.chdir(args.d)
+bd2 = TreeList([treesim.birth_death_tree(birth_rate=1.0, death_rate=0.5,
+                                         num_extant_tips=100,
+                                         repeat_until_success=True)
+                for _ in range(100)])
+bd2.write_to_path('birth_death2.nwk', schema='newick')
+bd5 = TreeList([treesim.birth_death_tree(birth_rate=1.0, death_rate=0.2,
+                                         num_extant_tips=100,
+                                         repeat_until_success=True)
+                for _ in range(100)])
+bd5.write_to_path('birth_death5.nwk', schema='newick')
+taxa = TaxonNamespace(['T{}'.format(x) for x in range(1, 101)])
+king = TreeList([treesim.pure_kingman_tree(taxon_namespace=taxa)])
+king.write_to_path('kingman.nwk', schema='newick')

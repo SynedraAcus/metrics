60652d25476e65ec20ab1bc2587f60b0357621ec
synedraacus <alexeymorozov1991@gmail.com>
2018-06-13 13:07:15 +0800

    Fixed leaf number bug

--- generate_trees.py
+++ generate_trees.py
@@ -14,15 +14,15 @@ if not os.path.isdir(args.d):
     os.mkdir(args.d)
 os.chdir(args.d)
 bd2 = TreeList([treesim.birth_death_tree(birth_rate=1.0, death_rate=0.5,
-                                         num_extant_tips=100,
+                                         num_extant_tips=args.n,
                                          repeat_until_success=True)
                 for _ in range(100)])
 bd2.write_to_path('birth_death2.nwk', schema='newick')
 bd5 = TreeList([treesim.birth_death_tree(birth_rate=1.0, death_rate=0.2,
-                                         num_extant_tips=100,
+                                         num_extant_tips=args.n,
                                          repeat_until_success=True)
                 for _ in range(100)])
 bd5.write_to_path('birth_death5.nwk', schema='newick')
-taxa = TaxonNamespace(['T{}'.format(x) for x in range(1, 101)])
+taxa = TaxonNamespace(['T{}'.format(x) for x in range(1, args.n+1)])
 king = TreeList([treesim.pure_kingman_tree(taxon_namespace=taxa) for _ in range(100)])
 king.write_to_path('kingman.nwk', schema='newick')
